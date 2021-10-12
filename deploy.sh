c3cb0a424c8d8b32f1748ac85a63638895aa8c97
Ubuntu <ubuntu@ip-172-31-46-65.ap-southeast-2.compute.internal>
2021-10-12 16:43:02 +0000

    change deploy path

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/ansible-nginx/ansible-nginx/sample-playbooks
+cd ansible_terra_ngx_destiny
 terraform init
 terraform fmt
 terraform plan
@@ -11,6 +11,9 @@ cat terraform.tfstate | grep '"private_ip":' | cut -d '"' -f 4 >> env/hosts
 
 cat private_key >> ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+
+echo "sleeping.."
+sleep 2m 30s
 
 echo "|1|xGgpzdLRABgcupKI2FldqScDySk=|ICZuBEZ2zlltXgKDITpBRGpHie4= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHYgaDqrGvGFEBifOAAjn74hicQe4A22ycWT69SgM1T4v3ZnAqHU+gRpnnnk08M73952fIBYdnOvRlONrcXb9Ok=" > ~/.ssh/known_hosts
 
